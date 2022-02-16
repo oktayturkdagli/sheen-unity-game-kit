bf83c768d9b9be41a576768b4ccf30b1988cd3d2
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2022-02-16 15:24:09 +0300

    fix(level-controller): Converted to empty editor class.

--- Assets/Sheen/SheenEditor/SheenLevelController.cs
+++ Assets/Sheen/SheenEditor/SheenLevelController.cs
@@ -1,67 +1,17 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using UnityEditor;
+using UnityEngine;
 
-public class SheenLevelController : MonoBehaviour
+public class SheenLevelController : EditorWindow
 {
-    public int level;
-    public float health;
-    public Vector3 target;
-
-    public GameObject obj;
-    public Vector3 spawnPoint;
-
-    public void BuildObject()
-    {
-        Instantiate(obj, spawnPoint, Quaternion.identity);
-    }
-
-}
-
-[CustomEditor(typeof(SheenLevelController))]
-public class SheenLevelControllerEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-        SheenLevelController myScript = (SheenLevelController)target;
-        if (GUILayout.Button("Build Object"))
-        {
-            myScript.BuildObject();
-        }
-        EditorGUILayout.HelpBox("This is a help box", MessageType.Info);
-
-    }
-
-}
-
-public class SheenLevelControllerInspector : EditorWindow
-{
-    //----
-    string myString = "Hello World";
-    bool groupEnabled;
-    bool myBool = true;
-    float myFloat = 1.23f;
-    //----
-
     [MenuItem("Window/Sheen/Sheen Level Controller")]
-    public static void ShowWindow()
+    public static void Init()
     {
-        EditorWindow.GetWindow(typeof(SheenLevelControllerInspector));
+        GetWindow(typeof(SheenLevelController));
     }
 
     void OnGUI()
     {
-        var editor = Editor.CreateEditor(Selection.activeGameObject.GetComponent<SheenLevelController>());
-        editor.OnInspectorGUI();
 
-        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
-        myString = EditorGUILayout.TextField("Text Field", myString);
-
-        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-        myBool = EditorGUILayout.Toggle("Toggle", myBool);
-        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
-        EditorGUILayout.EndToggleGroup();
     }
-
-
 }
+
