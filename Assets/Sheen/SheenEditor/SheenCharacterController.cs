9505287ede5d42f32c732c4f543436dc2dd58de4
Oktay Türkdağlı <oktayturkdagli@gmail.com>
2022-02-16 15:23:57 +0300

    fix(character-controller): Converted to empty editor class.

--- Assets/Sheen/SheenEditor/SheenCharacterController.cs
+++ Assets/Sheen/SheenEditor/SheenCharacterController.cs
@@ -1,67 +1,17 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using UnityEditor;
+using UnityEngine;
 
-public class SheenCharacterController : MonoBehaviour
+public class SheenCharacterController : EditorWindow
 {
-    public int level;
-    public float health;
-    public Vector3 target;
-
-    public GameObject obj;
-    public Vector3 spawnPoint;
-
-    public void BuildObject()
-    {
-        Instantiate(obj, spawnPoint, Quaternion.identity);
-    }
-
-}
-
-[CustomEditor(typeof(SheenCharacterController))]
-public class SheenCharacterControllerEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        DrawDefaultInspector();
-        SheenCharacterController myScript = (SheenCharacterController)target;
-        if (GUILayout.Button("Build Object"))
-        {
-            myScript.BuildObject();
-        }
-        EditorGUILayout.HelpBox("This is a help box", MessageType.Info);
-
-    }
-
-}
-
-public class SheenCharacterControllerInspector : EditorWindow
-{
-    //----
-    string myString = "Hello World";
-    bool groupEnabled;
-    bool myBool = true;
-    float myFloat = 1.23f;
-    //----
-
     [MenuItem("Window/Sheen/Sheen Character Controller")]
-    public static void ShowWindow()
+    public static void Init()
     {
-        EditorWindow.GetWindow(typeof(SheenCharacterControllerInspector));
+        GetWindow(typeof(SheenCharacterController));
     }
 
     void OnGUI()
     {
-        var editor = Editor.CreateEditor(Selection.activeGameObject.GetComponent<SheenCharacterController>());
-        editor.OnInspectorGUI();
 
-        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
-        myString = EditorGUILayout.TextField("Text Field", myString);
-
-        groupEnabled = EditorGUILayout.BeginToggleGroup("Optional Settings", groupEnabled);
-        myBool = EditorGUILayout.Toggle("Toggle", myBool);
-        myFloat = EditorGUILayout.Slider("Slider", myFloat, -3, 3);
-        EditorGUILayout.EndToggleGroup();
     }
-
-
 }
+
